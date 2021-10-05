4486f57328e653928cd24a631d14c39c4a08ff62
zdurr22 <zdurr22@gmail.com>
2021-10-05 15:36:35 -0400

    commit tree 6649a2744d464c868f07cc04daf0678c8e5bbf9c

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(drivers, string) {
+    return drivers.filter(n =>
+        n.toLowerCase() === string.toLowerCase()
+    )
+}
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(n => 
+    n.toLowerCase().indexOf(string.toLowerCase()) === 0 )
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(n => 
+        n.name === string)
+}
